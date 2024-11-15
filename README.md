b0366a8047ed2c2c8de57645efdf7123a0dd63f2
Lala <val.fort2911@gmail.com>
2024-11-15 18:29:20 +0800

    On branch main Initial commit new file:   README.md new file:   main.py

---
+++ README.md
@@ -0,0 +1,5 @@
+- Relearning C++ basic and converting them to python3 to avoid forgetting the basic of py3
+
+# Tutorial video -> https://www.youtube.com/watch?v=_bYFu9mBnr4&list=PPSV
+
+- stopped time 1:09:09
---
+++ main.py
@@ -0,0 +1,10 @@
+print("Hello Nikki")
+slices = input("Pick a number\n")
+print("Great! You'll get " + str(slices) + " slices of pizza.")
+
+# How to create a function
+def multiply(x, y):
+    return x * y
+
+x = multiply(5, 2)
+print("The output of integer x is " + str(x) + ".")
