b843293f1a2efe66635ea8ca4978e822aa9544c5
valFortD <val.fort2911@gmail.com>
2025-04-20 13:27:01 +0800

    modified:   course.py

--- course.py
+++ course.py
@@ -83,3 +83,50 @@ for asset in computer_assets:
         print("Keyboard found. Skipping to the next asset...")
         continue
     print(asset)
+
+openClose()
+# function for greeting employee
+def greet_employee(first_name, last_name):
+    print("Welcome! You're logged in", first_name, last_name + ".")
+greet_employee("Kiara", "Carter")
+
+openClose()
+# Use information returned from a function
+def calculate_fails(total_attempts, failed_attempts):
+    fail_percentage = (failed_attempts / total_attempts) * 100
+    return fail_percentage
+
+percentage = calculate_fails(4, 2)
+
+if percentage >= .5:
+    print("Account locked.")
+
+openClose()
+# return keyword is used to return information from a function
+def remaining_login_attempts(maximum_attempts, total_attempts):
+    return maximum_attempts - total_attempts
+remaining_attempts = remaining_login_attempts(3, 3)
+if remaining_attempts <= 0:
+    print("Your account is locked")
+
+openClose()
+# Use the sorted function
+usernames = ["elarson", "bmoreno", "tshah", "sgilmore", "eraab"]
+sorted_usernames = sorted(usernames)
+print("Sorted usernames:", sorted_usernames)
+print("Original usernames:", usernames)
+
+openClose()
+# Use max() and min() functions
+time_list = [12, 2, 32, 19, 57, 22, 14]
+print(min(time_list))
+print(max(time_list))
+
+openClose()
+# Calculate the average of number of failed login attempts per month for a particular user
+import statistics
+monthly_failed_attempts = [20, 17, 178, 33, 15, 21, 19, 29, 32, 15, 25, 19]
+mean_failed_attempts = statistics.mean(monthly_failed_attempts)
+print("mean:", mean_failed_attempts)
+median_failed_attempts = statistics.median(monthly_failed_attempts)
+print("median:", median_failed_attempts)
